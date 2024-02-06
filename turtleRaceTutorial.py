22e6a41c255471f402807a393995594df518594e
Aditya Mishra <mishraaaditya29@gmail.com>
2024-02-06 21:30:08 +0530

    made working code
    Previous code gave error "attributeerror: 'int' object has no attribute 'isdigit'"
    Remove isdigit() from line 12 and made working code

--- turtleRaceTutorial.py
+++ turtleRaceTutorial.py
@@ -3,54 +3,60 @@ import time
 import random
 
 WIDTH, HEIGHT = 700, 600
-COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 'pink', 'brown', 'cyan']
+COLORS = ['red', 'green', 'blue', 'orange', 'yellow',
+          'black', 'purple', 'pink', 'brown', 'cyan']
+
 
 def get_number_of_racers():
-	racers = 0
-	while True:
-		racers = input('Enter the number of racers (2 - 10): ')
-		if racers.isdigit():
-			racers = int(racers)
-		else:
-			print('Input is not numeric... Try Again!')
-			continue
+    racers = 0
+    while True:
+        racers = input('Enter the number of racers (2 - 10): ')
+        if racers:
+            racers = int(racers)
+        else:
+            print('Input is not numeric... Try Again!')
+            continue
 
-		if 2 <= racers <= 10:
-			return racers
-		else:
-			print('Number not in range 2-10. Try Again!')
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print('Number not in range 2-10. Try Again!')
+
 
 def race(colors):
-	turtles = create_turtles(colors)
+    turtles = create_turtles(colors)
 
-	while True:
-		for racer in turtles:
-			distance = random.randrange(1, 20)
-			racer.forward(distance)
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
 
-			x, y = racer.pos()
-			if y >= HEIGHT // 2 - 10:
-				return colors[turtles.index(racer)]
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 10:
+                return colors[turtles.index(racer)]
+
 
 def create_turtles(colors):
-	turtles = []
-	spacingx = WIDTH // (len(colors) + 1)
-	for i, color in enumerate(colors):
-		racer = turtle.Turtle()
-		racer.color(color)
-		racer.shape('turtle')
-		racer.left(90)
-		racer.penup()
-		racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
-		racer.pendown()
-		turtles.append(racer)
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape('turtle')
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
 
-	return turtles
+    return turtles
+
 
 def init_turtle():
-	screen = turtle.Screen()
-	screen.setup(WIDTH, HEIGHT)
-	screen.title('Turtle Racing!')
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title('Turtle Racing!')
+
 
 racers = get_number_of_racers()
 init_turtle()
